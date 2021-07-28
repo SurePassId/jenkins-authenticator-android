314495d8ef2f84a1ea18e3770eb5c27a134c07e2
Mirko J. Ploch <mirko.ploch@surepassid.com>
2021-07-28 15:32:21 -0400

    .

--- authenticator-android-coverity.groovy
+++ authenticator-android-coverity.groovy
@@ -7,6 +7,9 @@ pipeline {
     GIT_BRANCH='master'
     JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_301.jdk/Contents/Home'
     COVERITY_TOOL_HOME = '/Applications/cov-analysis-macosx-2021.06'
+    COV_URL = 'https://cov.surepassid.com:8443/'
+    COV_PROJECT = 'android-authenticator'
+    COV_STREAM = 'android-authenticator-surepassid-develop'
   }
 
   stages {
@@ -14,24 +17,22 @@ pipeline {
       steps {
         // Jenkins cleans the workspace
         cleanWs()
-        git branch: "develop",
+        git branch: "${GIT_BRANCH}",
             url: 'authenticator-android.github.com:SurePassId/Authenticator-Android.git'
       }
     }
     stage('Analyze') {
       steps {
         configFileProvider([configFile(fileId: '63bf33e9-8afc-4409-ba55-89f9ec7744bb', targetLocation: './local.properties')]) {
-          //sh '''PATH=/Applications/cov-analysis-macosx-2021.03/bin:${PATH}'''
           sh '''./coverity.sh'''
         }
       }
     }
     stage('Issue Check') {
       steps {
-        withCoverityEnvironment(coverityInstanceUrl: 'https://cov.surepassid.com:8443/',
-                                createMissingProjectsAndStreams: true,
-                                projectName: 'android-authenticator',
-                                streamName: 'android-authenticator-surepassid-develop',
+        withCoverityEnvironment(coverityInstanceUrl: "${COV_URL}",
+                                projectName: "${COV_URL}",
+                                streamName: "${COV_STREAM}",
                                 viewName: 'High Impact Outstanding') {
           coverityIssueCheck returnIssueCount: true,
                              markUnstable: true
